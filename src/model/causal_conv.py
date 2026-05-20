bonuses/exps (causal SoundStream, no-GAN training), fixes (audio logging)

src/model/causal_conv.py
@@ -3,9 +3,29 @@ import torch.nn.functional as F
 
 
 class CausalConv1d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, stride=1, padding=0):
+    """1D conv with causal or symmetric ("same") padding.
+
+    Total padding stays the same in both modes, so output lengths match.
+    """
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        dilation=1,
+        stride=1,
+        padding=0,
+        causal=True,
+    ):
         super().__init__()
-        self.padding_left = (kernel_size - 1) * dilation
+        self.causal = causal
+        total_pad = (kernel_size - 1) * dilation
+        if causal:
+            self.pad = (total_pad, 0)
+        else:
+            left = total_pad // 2
+            self.pad = (left, total_pad - left)
         self.conv = nn.Conv1d(
             in_channels=in_channels,
             out_channels=out_channels,
@@ -16,14 +36,17 @@ class CausalConv1d(nn.Module):
         )
 
     def forward(self, x):
-        return self.conv(F.pad(x, (self.padding_left, 0)))
+        return self.conv(F.pad(x, self.pad))
 
 
 class CausalConvTranspose1d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1):
+    """Transposed 1D conv with causal or symmetric trimming."""
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, causal=True):
         super().__init__()
+        self.causal = causal
         self.stride = stride
-        self.trim_right = kernel_size - stride
+        self.trim_total = kernel_size - stride
         self.conv_transpose = nn.ConvTranspose1d(
             in_channels=in_channels,
             out_channels=out_channels,
@@ -34,6 +57,11 @@ class CausalConvTranspose1d(nn.Module):
 
     def forward(self, x):
         x = self.conv_transpose(x)
-        if self.trim_right > 0:
-            x = x[..., :-self.trim_right]
+        if self.trim_total > 0:
+            if self.causal:
+                x = x[..., : -self.trim_total]
+            else:
+                trim_left = self.trim_total // 2
+                trim_right = self.trim_total - trim_left
+                x = x[..., trim_left : x.shape[-1] - trim_right]
         return x

src/model/decoder.py
@@ -6,17 +6,18 @@ from src.model.residual_unit import ResidualUnit
 
 
 class DecoderBlock(nn.Module):
-    def __init__(self, N, S):
+    def __init__(self, N, S, causal=True):
         super().__init__()
         self.conv_transpose = CausalConvTranspose1d(
             in_channels=N,
             out_channels=N // 2,
             kernel_size=2 * S,
             stride=S,
+            causal=causal,
         )
-        self.res_unit1 = ResidualUnit(N // 2, dilation=1)
-        self.res_unit2 = ResidualUnit(N // 2, dilation=3)
-        self.res_unit3 = ResidualUnit(N // 2, dilation=9)
+        self.res_unit1 = ResidualUnit(N // 2, dilation=1, causal=causal)
+        self.res_unit2 = ResidualUnit(N // 2, dilation=3, causal=causal)
+        self.res_unit3 = ResidualUnit(N // 2, dilation=9, causal=causal)
 
     def forward(self, x):
         x = self.conv_transpose(F.elu(x))
@@ -27,16 +28,18 @@ class DecoderBlock(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, C, K):
+    def __init__(self, C, K, causal=True):
         super().__init__()
-        self.conv1 = CausalConv1d(kernel_size=7, in_channels=K, out_channels=16 * C)
-        self.decoder_blocks = nn.Sequential(
-            DecoderBlock(16 * C, 5),
-            DecoderBlock(8 * C, 5),
-            DecoderBlock(4 * C, 4),
-            DecoderBlock(2 * C, 2),
+        self.conv1 = CausalConv1d(
+            kernel_size=7, in_channels=K, out_channels=16 * C, causal=causal
         )
-        self.conv2 = CausalConv1d(kernel_size=7, in_channels=C, out_channels=1)
+        self.decoder_blocks = nn.Sequential(
+            DecoderBlock(16 * C, 5, causal=causal),
+            DecoderBlock(8 * C, 5, causal=causal),
+            DecoderBlock(4 * C, 4, causal=causal),
+            DecoderBlock(2 * C, 2, causal=causal),
+        )
+        self.conv2 = CausalConv1d(kernel_size=7, in_channels=C, out_channels=1, causal=causal)
 
     def forward(self, x):
         x = self.conv1(x)

src/model/encoder.py
@@ -6,17 +6,18 @@ from src.model.residual_unit import ResidualUnit
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, N, S):
+    def __init__(self, N, S, causal=True):
         super().__init__()
         self.elu = nn.ELU()
-        self.res_unit1 = ResidualUnit(N // 2, dilation=1)
-        self.res_unit2 = ResidualUnit(N // 2, dilation=3)
-        self.res_unit3 = ResidualUnit(N // 2, dilation=9)
+        self.res_unit1 = ResidualUnit(N // 2, dilation=1, causal=causal)
+        self.res_unit2 = ResidualUnit(N // 2, dilation=3, causal=causal)
+        self.res_unit3 = ResidualUnit(N // 2, dilation=9, causal=causal)
         self.conv = CausalConv1d(
             kernel_size=2 * S,
             in_channels=N // 2,
             out_channels=N,
             stride=S,
+            causal=causal,
         )
 
     def forward(self, x):
@@ -28,16 +29,18 @@ class EncoderBlock(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, C, K):
+    def __init__(self, C, K, causal=True):
         super().__init__()
-        self.conv1 = CausalConv1d(kernel_size=7, in_channels=1, out_channels=C)
+        self.conv1 = CausalConv1d(kernel_size=7, in_channels=1, out_channels=C, causal=causal)
         self.encoder_blocks = nn.Sequential(
-            EncoderBlock(2 * C, 2),
-            EncoderBlock(4 * C, 4),
-            EncoderBlock(8 * C, 5),
-            EncoderBlock(16 * C, 5),
+            EncoderBlock(2 * C, 2, causal=causal),
+            EncoderBlock(4 * C, 4, causal=causal),
+            EncoderBlock(8 * C, 5, causal=causal),
+            EncoderBlock(16 * C, 5, causal=causal),
         )
-        self.conv2 = CausalConv1d(kernel_size=3, in_channels=16 * C, out_channels=K)
+        self.conv2 = CausalConv1d(
+            kernel_size=3, in_channels=16 * C, out_channels=K, causal=causal
+        )
 
     def forward(self, x):
         x = self.conv1(x)

src/model/residual_unit.py
@@ -4,11 +4,15 @@ from src.model.causal_conv import CausalConv1d
 
 
 class ResidualUnit(nn.Module):
-    def __init__(self, N, dilation):
-        super().__init__() 
+    def __init__(self, N, dilation, causal=True):
+        super().__init__()
         self.elu = nn.ELU()
-        self.conv1 = CausalConv1d(kernel_size=7, in_channels=N, out_channels=N, dilation=dilation)
-        self.conv2 = CausalConv1d(kernel_size=1, in_channels=N, out_channels=N)
+        self.conv1 = CausalConv1d(
+            kernel_size=7, in_channels=N, out_channels=N, dilation=dilation, causal=causal
+        )
+        self.conv2 = CausalConv1d(
+            kernel_size=1, in_channels=N, out_channels=N, causal=causal
+        )
 
     def forward(self, x):
         return x + self.conv2(self.elu(self.conv1(self.elu(x))))

src/model/soundstream.py
@@ -3,11 +3,20 @@ from src.model.encoder import Encoder
 from src.model.decoder import Decoder
 from src.model.rvq import ResidualVectorQuantizer
 
+
 class SoundStream(nn.Module):
-    def __init__(self, channels=32, num_quantizers=8, num_embeddings=1024, embedding_dim=128):
+    def __init__(
+        self,
+        channels=32,
+        num_quantizers=8,
+        num_embeddings=1024,
+        embedding_dim=128,
+        causal=True,
+    ):
         super().__init__()
-        self.encoder = Encoder(channels, embedding_dim)
-        self.decoder = Decoder(channels, embedding_dim)
+        self.causal = causal
+        self.encoder = Encoder(channels, embedding_dim, causal=causal)
+        self.decoder = Decoder(channels, embedding_dim, causal=causal)
         self.rvq = ResidualVectorQuantizer(num_quantizers, num_embeddings, embedding_dim)
 
     def forward(self, x):